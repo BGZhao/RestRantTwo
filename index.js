0b25c6a781be3963e4cc456f3e829df4cd395b23
BGZhao <92346786+BGZhao@users.noreply.github.com>
2022-03-04 23:51:51 -0600

    upload part three

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ const app = express()
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
-app.use('/places', require('./controllers/places'));
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-  res.send('Hello world home page')
+  res.render('home')
 })
 
 app.get('*', (req, res) => {
-  res.send('404 page')
+  res.render('error404')
 })
-
+// GET /places
+app.get('/', (req, res) => {
+  let places = []
+  res.render('places/index')
+})
 app.listen(process.env.PORT)
--- views/default.jsx
+++ views/default.jsx
@@ -12,5 +12,4 @@ function Def (html) {
         </html>
     )
   }
-
 module.exports = Def
